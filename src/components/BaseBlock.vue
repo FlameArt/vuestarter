3e224112a9b17631e2ce37685831a683436369d6
Flame <flameartme@gmail.com>
2022-02-04 23:42:24 +0300

    готово всё основное

--- src/components/BaseBlock.vue
+++ src/components/BaseBlock.vue
@@ -5,24 +5,36 @@
 // Тут находится обёртка
 div.block( block, :class="isSelected ? 'SelectedBox' : ''", @mousedown="dragMouseDown($event)", :style="'top:' + block.Y + 'px; left: ' + block.X + 'px; width:'+block.width+'px;height:'+block.height+'px;'")
   slot(name="Content")
-    div TestContent
+    div {{block.X + 'x' + block.Y}}
 
 </template>
 
 <script>
 
+// Сколько пикселей должно быть до блока чтобы к нему прикрепиться
+const MOVE_PX_TO_STACK = 5;
+const IS_STACK = true;
+
 export default {
   name: "BaseBlock",
   props: {
+    // Ссылка на объект текущего блока в общем состоянии
     block: {
       type: Object,
       default: {
         width: 300,
         height: 300,
         X: 300,
-        Y: 300
+        Y: 300,
+        startX: 0,
+        startY: 0
       }
-    }
+    },
+    helpers: {
+      type: Object
+    },
+    // Ссылка на все блоки, чтобы видеть их позиции
+    Blocks: {}
   },
   data: function () {
     return {
@@ -32,7 +44,7 @@ export default {
         clientY: undefined,
         movementX: 0,
         movementY: 0,
-        test: 0
+        isStacked: false
       },
       element: null,
       blockElement: null,
@@ -60,15 +72,20 @@ export default {
       this.blockElement = event.target;
 
       // Если блок не найден, значит это баблинг из дочерних элемов, и нужно найти родителя
-      if(!this.blockElement.hasAttribute('block')) {
+      if (!this.blockElement.hasAttribute('block')) {
         let finded = this.blockElement;
-        while(finded=finded.parentNode) {
-          if(finded.hasAttribute('block')){
+        while (finded = finded.parentNode) {
+          if (finded.hasAttribute('block')) {
             this.blockElement = finded;
             break;
           }
         }
       }
+
+      let elemPos = this.blockElement.getBoundingClientRect();
+
+      this.block.startX = elemPos.x
+      this.block.startY = elemPos.y
 
       document.onmousemove = this.elementDrag
       document.onmouseup = this.closeDragElement
@@ -76,14 +93,95 @@ export default {
     elementDrag: function (event) {
       event.preventDefault();
       event.stopPropagation();
-      this.positions.movementX = this.positions.clientX - event.clientX
-      this.positions.movementY = this.positions.clientY - event.clientY
-      this.positions.clientX = event.clientX
-      this.positions.clientY = event.clientY
-      this.block.Y = (this.blockElement.offsetTop - this.positions.movementY);
-      this.block.X = (this.blockElement.offsetLeft - this.positions.movementX);
+
+      let calculatedY = (this.blockElement.offsetTop - this.positions.movementY);
+      let calculatedX = (this.blockElement.offsetLeft - this.positions.movementX);
+
+      // общие расчёты
+      let isStackedY = ((Math.abs(this.positions.clientY - event.clientY) > MOVE_PX_TO_STACK));
+      let isStackedX = ((Math.abs(this.positions.clientX - event.clientX) > MOVE_PX_TO_STACK));
+
+      // Пристыковываемся к границам других блоков
+      let needStopY = false;
+      let needStopX = false;
+      if (IS_STACK) {
+        for (const block of this.Blocks) {
+
+          // свой блок не трогаем
+          if (block === this.block) continue;
+
+          if (!isStackedY) {
+            //Верхняя к верхней
+            if (Math.abs(calculatedY - block.Y) < MOVE_PX_TO_STACK) {
+              calculatedY = block.Y;
+              this.positions.isStacked = true;
+              needStopY = true;
+            }
+            // Нижняя [перемещаемого блока] к верхней [статичного]
+            if (Math.abs(calculatedY + this.block.height - block.Y) < MOVE_PX_TO_STACK) {
+              calculatedY = block.Y - this.block.height;
+              this.positions.isStacked = true;
+              needStopY = true;
+            }
+            // Нижняя к нижней
+            if (Math.abs(calculatedY + this.block.height - (block.height + block.Y)) < MOVE_PX_TO_STACK) {
+              calculatedY = block.Y+block.height-this.block.height;
+              this.positions.isStacked = true;
+              needStopY = true;
+            }
+            // Верхняя [перемещаемого блока] к нижней [статичного]
+            if (Math.abs(calculatedY - (block.Y+block.height)) < MOVE_PX_TO_STACK) {
+              calculatedY = block.Y + block.height;
+              this.positions.isStacked = true;
+              needStopY = true;
+            }
+          }
+
+          if (!isStackedX) {
+            //Левая к левой
+            if (Math.abs(calculatedX - block.X) < MOVE_PX_TO_STACK) {
+              calculatedX = block.X;
+              this.positions.isStacked = true;
+              needStopX = true;
+            }
+            // Правая [перемещаемого блока] к левой [статичного]
+            if (Math.abs(calculatedX + this.block.width - block.X) < MOVE_PX_TO_STACK) {
+              calculatedX = block.X - this.block.width;
+              this.positions.isStacked = true;
+              needStopX = true;
+            }
+            // Правая к правой
+            if (Math.abs(calculatedX + this.block.width - (block.width + block.X)) < MOVE_PX_TO_STACK) {
+              calculatedX = block.X+block.width-this.block.width;
+              this.positions.isStacked = true;
+              needStopX = true;
+            }
+            // Левая [перемещаемого блока] к правой [статичного]
+            if (Math.abs(calculatedX - (block.X+block.width)) < MOVE_PX_TO_STACK) {
+              calculatedX = block.X + block.width;
+              this.positions.isStacked = true;
+              needStopX = true;
+            }
+          }
+
+        }
+      }
+
+      this.block.Y = calculatedY;
+      this.block.X = calculatedX;
+
+      if (!needStopY) {
+        this.positions.movementY = this.positions.clientY - event.clientY
+        this.positions.clientY = event.clientY
+      }
+
+      if (!needStopX) {
+        this.positions.movementX = this.positions.clientX - event.clientX
+        this.positions.clientX = event.clientX
+      }
+
     },
-    closeDragElement () {
+    closeDragElement() {
       document.onmouseup = null
       document.onmousemove = null
     }
--- src/components/SmartBoard.vue
+++ src/components/SmartBoard.vue
@@ -4,47 +4,96 @@ div#SmartBoard
   // Это свободное пространство, по которому будут двигать основную доску
   div.SpaceBoard
     // Это основная доска, которую можно двигать
-    div.MainBoard(MainBoard, @mousedown="dragMouseDown($event)", :style="'height:' + board.height + 'px; width: ' + board.width + 'px;'")
-      BaseBlock.BaseBlock(v-for="block in Blocks", :block="block")
+    div.MainBoard(MainBoard, @mousedown="dragMouseDown($event)", :style="'top:' + board.Y + 'px; left: ' + board.X + 'px;height:' + board.height + 'px; width: ' + board.width + 'px;'")
+      BaseBlock.BaseBlock(v-for="thisBlock in Blocks", :block="thisBlock", :Blocks="Blocks", :helpers="blockHelpers", :style="thisBlock.isSelected ? 'border: 1px solid #ff0000' : 'border: 1px solid transparent'")
+
+      // Горизонтальная линия для подсказки куда пристыковаться
+      div.helperHorizontal(:style="'top:' + blockHelpers.horizontal.Y + 'px; left: ' + blockHelpers.horizontal.X + 'px;height:' + blockHelpers.horizontal.height + 'px; width: ' + blockHelpers.horizontal.width + 'px;'")
+      div.helperHorizontal(:style="'top:' + blockHelpers.horizontal.Y + 'px; left: ' + blockHelpers.horizontal.X + 'px;height:' + blockHelpers.horizontal.height + 'px; width: ' + blockHelpers.horizontal.width + 'px;'")
+
+      div.helperSelect(v-show="this.blockHelpers.selector.isShow", ref="helperSelect",:style="'top:' + blockHelpers.selector.top + 'px; left: ' + blockHelpers.selector.left + 'px;height:' + blockHelpers.selector.height + 'px; width: ' + blockHelpers.selector.width + 'px;'")
+        div {{this.blockHelpers.selector.startX + 'x' + this.blockHelpers.selector.startY + ' - ' + (this.blockHelpers.selector.width + this.blockHelpers.selector.startX)}}
 </template>
 
 <script>
 import BaseBlock from "./BaseBlock.vue";
+
 export default {
-    name: "SmartBoard",
-    components: {BaseBlock},
-    data: function () {
-        return {
+  name: "SmartBoard",
+  components: {BaseBlock},
+  data: function () {
+    return {
 
-          Blocks: [],
+      Blocks: [],
 
-          positions: {
-            clientX: undefined,
-            clientY: undefined,
-            movementX: 0,
-            movementY: 0,
-            test: 0
-          },
+      positions: {
+        clientX: undefined,
+        clientY: undefined,
+        movementX: 0,
+        movementY: 0,
+        test: 0
+      },
 
-          board: {
-            height: 2600,
-            width: 2600,
-            X: 0,
-            Y: 0,
-          },
+      board: {
+        height: 10000,
+        width: 10000,
+        X: -3500,
+        Y: -4000,
+      },
 
-          element: null,
-          blockElement: null,
-
-          isSelected: false
+      blockHelpers: {
+        // Линии для подсказок
+        horizontal: [{
+          height: 1,
+          width: 0,
+          X: 0,
+          Y: 0,
+        }],
+        vertical: [{
+          height: 0,
+          width: 1,
+          X: 0,
+          Y: 0,
+        }],
+        // Бокс для выделения элементов
+        selector: {
+          startX: 0,
+          startY: 0,
+          left: 0,
+          top: 0,
+          X: 0,
+          Y: 0,
+          height: 0,
+          width: 0,
+          isShow: false,
         }
-    },
+      },
+
+      SelectedBlocks: [],
+
+      element: null,
+      blockElement: null,
+
+      isSelected: false
+    }
+  },
   mounted() {
     this.Blocks.push({
       height: 300,
       width: 300,
-      X: 500,
-      Y: 500,
+      X: -this.board.X,
+      Y: -this.board.Y + 300,
+      startX: 0,
+      startY: 0,
+      isSelected: false
+    }, {
+      height: 200,
+      width: 200,
+      X: -this.board.X + 500,
+      Y: -this.board.Y + 300,
+      startX: 0,
+      startY: 0,
+      isSelected: false
     })
   },
   methods: {
@@ -59,17 +108,33 @@ export default {
       this.blockElement = event.target;
 
       // Если блок не найден, значит это баблинг из дочерних элемов, и нужно найти родителя
-      if(!this.blockElement.hasAttribute('MainBoard')) {
+      if (!this.blockElement.hasAttribute('MainBoard')) {
         let finded = this.blockElement;
-        while(finded=finded.parentNode) {
-          if(finded.hasAttribute('MainBoard')){
+        while (finded = finded.parentNode) {
+          if (finded.hasAttribute('MainBoard')) {
             this.blockElement = finded;
             break;
           }
         }
       }
 
-      document.onmousemove = this.elementDrag
+      switch (event.button) {
+        // Левой кнопкой мыши селектим
+        case 0:
+          this.blockHelpers.selector.startX = (Math.abs(this.blockElement.offsetLeft) + event.clientX);
+          this.blockHelpers.selector.startY = (Math.abs(this.blockElement.offsetTop) + event.clientY);
+          this.SelectedBlocks.splice(0);
+          this.blockHelpers.selector.isShow = true;
+          this.blockHelpers.selector.width = 0;
+          this.blockHelpers.selector.height = 0;
+          document.onmousemove = this.selectorBox;
+          break;
+        // Правой и средней кнопкой мыши перемещаем
+        case 1:
+        case 2:
+          document.onmousemove = this.elementDrag;
+          break;
+      }
       document.onmouseup = this.closeDragElement
     },
     elementDrag: function (event) {
@@ -81,21 +146,78 @@ export default {
       let calcTop = (this.blockElement.offsetTop - this.positions.movementY);
       let calcLeft = (this.blockElement.offsetLeft - this.positions.movementX);
 
+      // Добавляем новое пространство
+      // TODO: можно улучшить производительность, а может и не нужно, а достаточно просто большого холста
+      // TODO: всё работает, только одна проблема: при перемещении елозит фон
+      /*
+      // сверху
       if(calcTop>-30) {
-        this.board.height += (30 + calcTop);
-        this.board.Y += (30 + calcTop);
+        calcTop = calcTop - 250;
+        this.board.height += 250;
+        for (const block of this.Blocks) {
+          block.Y += 250;
+        }
       }
-      else {
-        this.board.Y = calcTop;
+      // снизу
+      if(calcTop + this.board.height>window.innerHeight+30) {
+        this.board.height += 250;
+      }
+      // слева
+      if(calcLeft>-30) {
+        calcLeft = calcLeft - 250;
+        this.board.width += 250;
+        for (const block of this.Blocks) {
+          block.X += 250;
+        }
+      }
+      // справа
+      if(calcLeft + this.board.width>window.innerWidth+30) {
+        this.board.width += 250;
       }
 
-      this.blockElement.style.top = this.board.Y + 'px'
-      this.blockElement.style.left = calcLeft + 'px'
+       */
+
+      this.board.Y = calcTop;
+      this.board.X = calcLeft;
 
     },
-    closeDragElement () {
+    closeDragElement() {
       document.onmouseup = null
       document.onmousemove = null
+      this.blockHelpers.selector.isShow = false;
+    },
+
+    selectorBox: function (event) {
+
+      let movementX = this.positions.clientX - event.clientX
+      let movementY = this.positions.clientY - event.clientY
+      let calcTop = (Math.abs(this.blockElement.offsetTop) + event.clientY);
+      let calcLeft = (Math.abs(this.blockElement.offsetLeft) + event.clientX);
+      let calcBottom = this.blockHelpers.selector.startY + this.blockHelpers.selector.height;
+      let calcRight = this.blockHelpers.selector.startX + this.blockHelpers.selector.width;
+
+      this.blockHelpers.selector.top = calcTop - this.blockHelpers.selector.startY > 0 ? this.blockHelpers.selector.startY : calcTop;
+      this.blockHelpers.selector.left = calcLeft - this.blockHelpers.selector.startX > 0 ? this.blockHelpers.selector.startX : calcLeft;
+
+      //if(calcLeft - this.blockHelpers.selector.startX < 0) debugger;
+      this.blockHelpers.selector.width = Math.abs(calcLeft - this.blockHelpers.selector.startX);
+      this.blockHelpers.selector.height = Math.abs(calcTop - this.blockHelpers.selector.startY);
+
+      // определяем селекченные элемы
+      for (const block of this.Blocks) {
+        if(
+            this.blockHelpers.selector.left < block.X + block.width && calcRight > block.X &&
+            this.blockHelpers.selector.top < block.Y + block.height && calcBottom > block.Y ){
+          if(this.SelectedBlocks.find(r=>r===block)===undefined) {
+            this.SelectedBlocks.push(block);
+          }
+          block.isSelected = true;
+        }
+        else {
+          block.isSelected = false;
+        }
+      }
+
     }
   }
 }
@@ -122,11 +244,9 @@ export default {
       width: 2600px;
       height: 2600px;
       position: absolute;
-      left: -25%;
-      top: -25%;
       cursor: grab;
 
-      background: linear-gradient( #bbb, transparent 1px), linear-gradient( 90deg, #bbb, transparent 1px);
+      background: linear-gradient(#bbb, transparent 1px), linear-gradient(90deg, #bbb, transparent 1px);
       background-size: 150px 150px;
       background-position: center center;
 
@@ -134,6 +254,28 @@ export default {
         background: #42b983;
         width: 300px;
         height: 300px;
+      }
+
+      // Горизонтальная линия для подсказок стакинга
+      .helperHorizontal {
+        height: 1px;
+        background: gray;
+        position: absolute;
+      }
+
+      // Вертикальная линия для хелпера
+      .helperVertical {
+        height: 1px;
+        background: gray;
+        position: absolute;
+      }
+
+
+      // Селектор
+      .helperSelect {
+        background: blue;
+        opacity: 0.3;
+        position: absolute;
       }
 
     }
