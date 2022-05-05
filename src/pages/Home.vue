a63afddd89f7a05eb54eeaa7587f40d2a8eecbd7
Flame <flameartme@gmail.com>
2022-05-05 17:38:54 +0300

    new: hero icons included w extension, packages updated, extension recomenations added

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,10 +7,14 @@
 
 <script setup>
 
-import { onMounted } from "@vue/runtime-core";
+// Основные импорты
+import { onMounted, reactive } from "@vue/runtime-core";
 import { storeFile } from "/src/store";
 import { useRoute, useRouter } from "vue-router";
 import REST from "flamerest"
+
+// Иконки
+import { XCircleIcon } from '@heroicons/vue/solid'
 
 const store = storeFile(), router = useRouter(), route = useRoute();
 
