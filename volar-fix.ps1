531b6926c2492bfd4c68816e5768505fc51f404f
Flame <flameartme@gmail.com>
2022-05-16 13:36:25 +0300

    feat: volar autofix for windows

---
+++ volar-fix.ps1
@@ -0,0 +1,1 @@
+((Get-Content -path ("" + (gci $env:USERPROFILE/.vscode/extensions/*volar* | sort Name | select -last 1) + "/dist/node/server.js") -Raw).replace('/^\.([_a-z0-9\-]*[_a-z][_a-z0-9\-]*)/i', '/^\.([_a-z0-9\-]*[_a-z]([_a-z0-9\-\[\]]|:(?! ))*)/i')) | Set-Content -Path ("" + (gci $env:USERPROFILE/.vscode/extensions/*volar* | sort Name | select -last 1) + "/dist/node/server.js")
