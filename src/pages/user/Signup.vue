36cbc9125fa50397eaf736ca526f36e205ccd715
Flame <flameartme@gmail.com>
2022-05-21 18:25:12 +0300

    fix: signup

--- src/pages/user/Signup.vue
+++ src/pages/user/Signup.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .flex.justify-center.h-screen.items-center.flex-col
 
-  .px-8.py-6.mt-4.text-left.bg-white.shadow-lg.lg:w-[725px]
+  .px-8.py-6.mt-4.text-left.bg-white.shadow-lg.lg:w-feed
     
     // Регистрация через соц-сети
     .mt-0
@@ -30,17 +30,17 @@
           type="text",
           placeholder="Ваше имя или никнейм, который увидят другие",
         )
-        span.text-xs.tracking-wide.text-red-600 {{ state.errors['name'].join(". ") }}
+        span.text-xs.tracking-wide.text-red-600 {{ state.errors['name']?.join(". ") }}
 
       // Почта
-      label.block
+      label.block.mt-2
         input.w-full.px-4.py-2.mt-0.border.rounded-md.focus:outline-none.focus:ring-1.focus:ring-blue-600.placeholder:text-gray-500(
           v-model="state.email",
           name="email",
           type="text",
           placeholder="Почта",
         )
-        span.text-xs.tracking-wide.text-red-600 {{ state.errors['email'].join(". ") }}
+        span.text-xs.tracking-wide.text-red-600 {{ state.errors['email']?.join(". ") }}
       
       // Пароль
       label.block.mt-2
@@ -50,7 +50,7 @@
           type="password",
           placeholder="Пароль",
         )
-        span.my-4.text-xs.tracking-wide.text-red-600 {{ state.errors['password'].join(". ") }}
+        span.my-4.text-xs.tracking-wide.text-red-600 {{ state.errors['password']?.join(". ") }}
       
       // Кнопка реги
       .flex.items-center.justify-between.flex-col
