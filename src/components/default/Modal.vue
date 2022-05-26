899f9158174ed94adc582875a277ade1ea527e85
Flame <flameartme@gmail.com>
2022-05-26 11:58:28 +0300

    feat: universal modal added, components folder with default modal component

---
+++ src/components/default/Modal.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+
+// УНИВЕРСАЛЬНОЕ МОДАЛЬНОЕ ОКНО
+   Позволяет вызывать себя через show() и close()
+
+CustomModal(v-model="isShow" :close="close")
+  .bg-white.rounded-xl
+    // Этот слот позволяет заменить всё сразу
+    slot
+    
+      // Заголовок
+      .flex.items-center.px-5.border-b.border-b-gray-300(v-if="title!==''")
+        h1.text-3xl.py-5 {{title}}
+        XCircleIcon.ml-24.w-6.opacity-25.cursor-pointer(@click="close")
+
+      // Тело
+      .my-3.px-5
+        slot(name="desc")
+          .text-left {{desc === '' ? 'Укажите сообщение или используйте слот' : ''}}
+      
+      // Кнопки
+      slor(name="buttons")
+        .border-t.border-t-gray-300.mb-4.pt-3.pr-5.flex.items-end.flex-col
+          button.cursor-pointer.ml-5.rounded-full.bg-blue-700.px-4.py-1.text-white(@click="close") {{closeButton === '' ? 'Закрыть' : closeButton}}
+      
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from '@vue/runtime-core'; import { storeFile } from "@/store"; import { useRoute, useRouter } from 'vue-router'; import REST from "flamerest";
+
+// Иконки
+import { XCircleIcon } from '@icons/solid'
+
+export default defineComponent({
+
+  components: {XCircleIcon},
+
+  props: {
+    title: {
+      type: String,
+      default: "",
+      required: false
+    },
+    desc: {
+      type: String,
+      default: "",
+      required: false
+    },
+    closeButton: {
+      type: String,
+      default: "",
+      required: false
+    },
+  },
+
+  setup(props) {
+
+    const isShow = ref(false)
+
+    function show() {
+      isShow.value = true
+    }
+
+    function close() {
+      isShow.value = false
+    }
+
+    return {
+      isShow,
+      show,
+      close,
+    }
+  },
+
+})
+
+
+</script>
+
+<style scoped lang="scss">
+</style>
