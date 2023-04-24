5235eeb52a22d86e7885b38f2ef7b7c028a31f4e
Flame <flameartme@gmail.com>
2023-04-25 01:04:26 +0400

    feat: install update android

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flameresttable/src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
